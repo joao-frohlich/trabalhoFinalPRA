5619baf2c89303b0acf21d1db44378f247e18f5c
joao-frohlich <joaovitorfrohlich@gmail.com>
2021-03-31 15:24:22 -0300

    Criando repositório para trabalho de PRA

---
+++ cmake-build-debug/CMakeFiles/trabalhoFinalPRA.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/trabalhoFinalPRA.dir/main.c.o"
+  "trabalhoFinalPRA"
+  "trabalhoFinalPRA.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/trabalhoFinalPRA.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
