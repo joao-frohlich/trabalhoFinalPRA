08df93fc35963409dced7076c7f5dc2e575fca4e
Machado <elitonmachadod200@gmail.com>
2021-04-06 23:01:24 -0300

    plot seaborn

--- tests/plot.py
+++ tests/plot.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
+import pandas as pd
 import seaborn as sns
-
 
 plt.rcParams.update({'font.size': 20})  # font size
 fig_size = (19.2, 12)
@@ -12,44 +12,44 @@ def make_plot(title, avl, b):
     plt.figure(figsize=fig_size, constrained_layout=True)
     sns.set_style("darkgrid")
 
-    plt.plot(avl, label="AVL")
-    plt.plot(b, label="B")
+    sns.lineplot(x="Sz", y="Avl", data=avl, label="AVL")
+    sns.lineplot(x="Sz", y="B", data=b, label="B")
 
     plt.title(title)
     plt.ylabel('Esforço Computacional')
     plt.xlabel('Tamanho da entrada')
 
-    plt.legend()
     plt.savefig(path + title + ".png")
 
 
 def get_data_worst():
-    avl = [0]
-    b = [0]
+    avl = []
+    b = []
     for sz in range(1, 101):
         path = f"./worst/sz{sz:03d}/"
         file_name = f"t0sz{sz:03d}.out.txt"
         with open(path + file_name, "r") as file:
-            avl.append(int(file.readline()))
-            b.append(int(file.readline()))
+            avl.append([sz, int(file.readline())])
+            b.append([sz, int(file.readline())])
             file.close()
+    avl = pd.DataFrame(avl, columns=["Sz", "Avl"])
+    b = pd.DataFrame(b, columns=["Sz", "B"])
     return avl, b
 
 
 def get_data_random():
-    avl = [0]
-    b = [0]
+    avl = []
+    b = []
     for sz in range(1, 101):
         path = f"./random/sz{sz:03d}/"
-        avl_sum, b_sum = 0, 0
         for t in range(10):
             file_name = f"t{t}sz{sz:03d}.out.txt"
             with open(path + file_name, "r") as file:
-                avl_sum += int(file.readline())
-                b_sum += int(file.readline())
+                avl.append([sz, int(file.readline())])
+                b.append([sz, int(file.readline())])
                 file.close()
-        avl.append(avl_sum / 10)
-        b.append(b_sum / 10)
+    avl = pd.DataFrame(avl, columns=["Sz", "Avl"])
+    b = pd.DataFrame(b, columns=["Sz", "B"])
     return avl, b
 
 
